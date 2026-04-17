ed4207d0ba9f4be7f5406ff15a584ca8fdcecb31
Cedric <cedric_daniel_chan@dlsu.edu.ph>
2026-04-17 18:38:44 +0800

    Add findingWin logic

--- game.js
+++ game.js
@@ -13,6 +13,8 @@ Gameboard Object
 
 *** Methods
 ** addPosition - deals with adding a player's position to the board. This assumes it is valid.
+** numOfFreeSpace - returns the number of unoccupied cells
+** findWin - checks if there is already a winning combination
 */
 function Gameboard(){
     const board = [];
@@ -22,13 +24,38 @@ function Gameboard(){
         board[i] = '0';
     }
 
-    const addPosition = (symbol, chosenPos) =>{
+    const addPosition = (symbol, chosenPos) => {
         board[chosenPos] = symbol;
     };
 
+    let numOfFreeSpace = () => {
+       return (board.filter((position) => position === '0')).length;
+    };
+
+    let findWin = () => {
+        let isWin;
+
+        if ((board[0] != '0' && board[1] != '0' && board[2] != '0') && (board[0] == board[1] && board[0] == board[2]) ||
+            (board[3] != '0' && board[4] != '0' && board[5] != '0') && (board[3] == board[4] && board[3] == board[5]) ||
+            (board[6] != '0' && board[7] != '0' && board[8] != '0') && (board[6] == board[7] && board[6] == board[8]) ||
+            (board[0] != '0' && board[3] != '0' && board[6] != '0') && (board[0] == board[3] && board[0] == board[6]) ||
+            (board[1] != '0' && board[4] != '0' && board[7] != '0') && (board[1] == board[4] && board[1] == board[7]) ||
+            (board[2] != '0' && board[5] != '0' && board[8] != '0') && (board[2] == board[5] && board[2] == board[8]) ||
+            (board[0] != '0' && board[4] != '0' && board[8] != '0') && (board[0] == board[4] && board[0] == board[8]) ||
+            (board[2] != '0' && board[4] != '0' && board[6] != '0') && (board[2] == board[4] && board[2] == board[6])
+           ){
+            isWin = true;
+           }
+        else{
+            isWin = false;
+        }
+        return isWin;
+    };
 
     return {
-        addPosition
+        addPosition,
+        numOfFreeSpace,
+        findWin
     };
 
 }
@@ -41,13 +68,20 @@ Player Object
 
 *** Variables:
 ** playerSymbol is the symbol used by the player
-** playerName is the chosen name of the player (TO BE ADDED)
+** playerName is the chosen name of the player
+
+*** Methods
+** getPlayerSymbol - a setter to returning the playerSymbol
 */
 function Player(symbol){
     let playerSymbol = symbol;
     const playerName = prompt(`Player ${playerSymbol} what is your name? `);
 
-    return {playerSymbol, playerName};
+    let getPlayerSymbol = () => {
+        return playerSymbol;
+    };
+
+    return {getPlayerSymbol};
 }
 
 
@@ -59,7 +93,7 @@ Game Logic Object
 ** It checks for if a player has won a game already
 
 *** Variables:
-** currentPlayer determines which player turn it is
+** currentPlayer determines which player turn it is 0 is player1 and X is player2
 ** chosenPos stores the chosen position of the player
 ** player1 is an object
 ** player2 is an object
@@ -81,31 +115,49 @@ function Controller(){
         else
             console.log(player2.playerName);
     };
+
     let gameOver = () =>{
+        let numOfFreeSpace = gameboard.numOfFreeSpace;
+        let isWin = gameboard.findWin;
         // check if the positions are all filled
-
+        if (numOfFreeSpace === 0)
+            return true;
         // check if there is already a winning combination
-
+        else if (isWin)
+            return true;
         // else return false
+        else
+            return false;
     };
 
     const gameLoop = () =>{
         do{
             // Get the player's position
-            console.log('This works');
+            let playerSymbol;
+            let chosenPos;
+
+            if (currentPlayer === 0)
+                playerSymbol = player1.getPlayerSymbol();
+            else
+                playerSymbol = player2.getPlayerSymbol();
+
+            do{
+                chosenPos = prompt(`Player ${playerSymbol} move: `);
+            }while(gameboard[chosenPos] != '0');
+            
             // Update the board
 
             // Update the next player
+            currentPlayer = !currentPlayer;
 
-        }while(!gameOver);
+        }while(!gameOver());
     };
 
     return {
         showPlayerName,
-        gameboard,
         gameOver,
         gameLoop
     };
 }
 
-const game = Controller();
+const Game = Controller();
